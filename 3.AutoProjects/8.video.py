954fba8475048016b0db5c1b2966d73ac452aef4
Fu Liu <fliu@jestais.com>
2025-06-11 17:04:31 -0400

    061125-1

--- 3.AutoProjects/8.video.py
+++ 3.AutoProjects/8.video.py
@@ -37,3 +37,51 @@ video.set(1, frame_nr)
 success, frame = video.read()
 
 cv2.imwrite(f'Frame at {hours}:{minutes}:{seconds}.jpg', frame)
+
+# ---- Faces in Video ----
+import cv2
+
+video = cv2.VideoCapture("video.mp4") # read original video
+success, frame = video.read()
+
+height = frame.shape[0] # capture picture size
+width = frame.shape[1]
+
+face_cascade = cv2.CascadeClassifier('faces.xml') # face cascade
+output = cv2.VideoWriter('output.avi', cv2.VideoWriter_fourcc(*'DIVX'), 30, (width, height)) # output video
+
+while success:
+    faces = face_cascade.detectMultiScale(frame, 1.1, 4)
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 255, 255), 4) # draw rectangle in the face
+    output.write(frame)
+    success, frame = video.read()
+
+output.release() # generate video
+
+
+# ---- Faces Using Blurred Area ----
+import cv2
+
+video = cv2.VideoCapture("files/smile.mp4")
+
+success, frame = video.read() # read frame
+
+height = frame.shape[0]
+width = frame.shape[1]
+
+face_cascade = cv2.CascadeClassifier('faces.xml')
+
+output = cv2.VideoWriter('blurred_smile.mp4', cv2.VideoWriter_fourcc(*'DIVX'), 30, (width, height))
+
+while success:
+    faces = face_cascade.detectMultiScale(frame, 1.1, 4)
+
+    for (x, y, w, h) in faces:
+        frame[y:y+h, x:x+w] = cv2.blur(frame[y:y+h, x:x+w], (50, 50))
+
+    output.write(frame)
+
+    success, frame = video.read()
+
+output.release()
